9be1aea0619fad458eff824ac1f833feec273ff3
Tim Koval <timkoval00@gmail.com>
2025-02-01 18:27:37 +0400

    add some updates

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Welcome to Tim's Neurogarden
 ---
 
 This is a blank Quartz installation.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🏡 Tim's Neurodynamica",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "timkoval.rs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
